a62301001977ad032d8270378abac745e214072b
Bastien Durel <bastien@durel.org>
2012-02-02 22:38:47 +0100

    working

--- extension.js
+++ extension.js
@@ -2,33 +2,30 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Cairo = imports.cairo;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
 
-let text, button;
+const SETTINGS_SCHEMA = 'org.gnome.shell.extensions.lunar-clock';
+const POSITION_IN_PANEL_KEY = 'position-in-panel';
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+let clock;
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
+LunarClock.prototype = {
+	__proto__: PanelMenu.Button.prototype,
+	_init: function() {
+        // Load settings
+        this._settings = getSettings(SETTINGS_SCHEMA);
+		this._position_in_panel = this._settings.get_enum(POSITION_IN_PANEL_KEY);
 
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+        // Panel icon
+        this._icon = new St.Icon({
+            icon_type: this._icon_type,
+            icon_name: 'view-refresh-symbolic',
+            style_class: 'system-status-icon lunar-icon' + (Main.panel.actor.get_direction() == St.TextDirection.RTL ? '-rtl' : '')
+        });
+	}
+};
 
 function init() {
     button = new St.Bin({ style_class: 'panel-button',
